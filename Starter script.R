633f7d704f5902b89f31b0cda0d20d8ce482e1d4
Katherine S Nelson <ksnelson@ksu.edu>
2022-01-11 14:57:52 -0600

    intial commit, updated tutorial 1

---
+++ Starter script.R
@@ -0,0 +1,21 @@
+
+
+R.Version()
+
+citation()
+
+install.packages("tidyverse") #the universe of tidy data
+
+library(tidyverse)
+
+update.packages()
+
+?read.csv #get help info on reading in a csv file
+
+?hist
+
+??csv
+
+??histogram #search for help topics related to histograms
+
+#update your look in the Tools --> Global Options menu
